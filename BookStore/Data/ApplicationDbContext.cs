d2587f12f3e7a0e2d6197ad555a8553177638fcb
Tri Vo <trivmgcs210019@fpt.edu.vn>
2023-08-25 14:58:33 +0700

    Add project files.

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookStore.Models.ApplicationRole> ApplicationRole { get; set; } = default!;
+    }
+}
---
+++ BookStore/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public string? Descriptions;
+    }
+}
